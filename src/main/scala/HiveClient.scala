a9b26279c310fa1386bbf902913b740168c2e5cf
snehith <ksnehith@live.com>
2020-05-13 02:56:38 -0400

    initial commit

---
+++ src/main/scala/HiveClient.scala
@@ -0,0 +1,19 @@
+import java.sql.{Connection, DriverManager, ResultSet}
+
+
+object HiveClient extends App {
+  val driverName: String = "org.apache.hive.jdbc.HiveDriver"
+  Class.forName(driverName)
+
+  val connection: Connection = DriverManager
+    .getConnection("jdbc:hive2://172.16.129.58:10000/snehith;user=snehith;password=snehith")
+  val stmt = connection.createStatement()
+
+  val res: ResultSet = stmt.executeQuery("SELECT * FROM fall2019_snehith.logs_internal")
+  while (res.next()) {
+    println("userid: " + res.getString(3))
+  }
+  stmt.close()
+  connection.close()
+
+}
